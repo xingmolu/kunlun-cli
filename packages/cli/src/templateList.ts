38f8218e535771d399200a9c4634572c486ffc4f
行陌路 <591745264@qq.com>
2023-08-13 14:15:38 +0800

    support vite-ts-react & ts-node

--- packages/cli/src/templateList.ts
+++ packages/cli/src/templateList.ts
@@ -1,9 +1,9 @@
 export const TEMPLATE_LIST = [{
   name: 'vite-react-ts',
-  repoURL: 'https://github.com/xingmolu/ued-blog/archive/refs/heads/master.zip',
+  repoURL: 'https://github.com/TeXmeijin/vite-react-ts-tailwind-firebase-starter/archive/refs/heads/main.zip',
   useage: 'cd <project-name> && npm install && npm run dev'
 }, {
-  name: 'node',
-  repoURL: 'https://github.com/xingmolu/ued-blog/archive/refs/heads/master.zip',
+  name: 'node-ts',
+  repoURL: 'https://github.com/jsynowiec/node-typescript-boilerplate/archive/refs/heads/main.zip',
   useage: 'cd <project-name> && npm install && npm run dev'
 }];
